12518d4af42ce9e0ec3225396653e8ce735a8375
ACER <ACER@NGOCHAI>
2023-09-22 13:57:11 +0700

    BrandId

--- Web.DAL/Entities/Brands.cs
+++ Web.DAL/Entities/Brands.cs
@@ -8,7 +8,7 @@ namespace Web.DAL.Entities
 {
     public class Brands
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
         public bool IsDeleted { get; set; }
