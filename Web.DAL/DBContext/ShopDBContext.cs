0023ffdd099436df627ecde35d218cd0a2f7b313
Minh Nguyen <102134983+MinhNguyen0912@users.noreply.github.com>
2023-09-22 14:23:57 +0700

    update

---
+++ Web.DAL/DBContext/ShopDBContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.DAL.Configurations;
+using Web.DAL.Entities;
+
+namespace Web.DAL.DBContext
+{
+    public class ShopDBContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ShopDBContext()
+        {
+
+        }
+        public ShopDBContext(DbContextOptions<ShopDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Thêm các config vào chỗ này
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new RoleConfiguration());
+
+
+
+
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ShoppingMall;Trusted_Connection=True;TrustServerCertificate=true;");
+        }
+        //Thêm dbset vào chỗ này
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<ApplicationRole> Roles { get; set; }
+    }
+}
